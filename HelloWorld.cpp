dd8a56b0b78900f6d3775cab0f2124e0f19c0465
arpitran <35965795+arpitran@users.noreply.github.com>
2019-07-22 19:53:28 -0400

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,10 +1,21 @@
 // helloWorld.cpp by Arpit Rana
+/*
+Multi-line comments
+*/
 
 #include <iostream>
 #include <cstdio>
+using namespace std;
 
-int main() {
-  std::cout<< "Hello World"<<std::endl;
-  return 0;
 
+int main(){
+  int x;
+  x = 42;
+  printf("x is %d\n",x);
+  cout << "cout Hello, World!"<< endl;
+  const char * str = " string Hello, World!";
+  puts(str);
+  puts("puts Hello, World!");
+  printf("printf Hello, World!\n Hello");
+return 0;
 }
