b84fa6d6a13e5a543ec6ad433d29a6a7b65f118f
arpitran <35965795+arpitran@users.noreply.github.com>
2019-07-22 20:44:13 -0400

    Create Working.cpp

---
+++ Working.cpp
@@ -0,0 +1,22 @@
+// Working.cpp by Arpit Rana
+/*
+Multi-line comments
+*/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int main(){
+  int x = 7;
+  int y = 42;
+  int *ip = &x;
+
+  puts("Hello, World!");
+  printf("The value of x is %d\n",x);
+  printf("The value of y is %d\n",y);
+  printf("The value of *ip is %d\n",*ip);
+
+return 0;
+}
