ea021cfd95b20f35cdd4ab67c625c34477763506
arpitran <35965795+arpitran@users.noreply.github.com>
2019-07-24 03:32:36 -0400

    Create comparisionOperators.cpp

---
+++ Operators/comparisionOperators.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+using namespace std;
+
+int main(){
+  int x = 5;
+  int y = 42;
+
+  if(x == y){
+    puts("true");
+  }else{
+    puts("false");
+  }
+
+  if(x>=y){
+    puts("true");
+  }else{
+    puts("false");
+  }
+
+
+  puts("Hello, World!");
+  return 0;
+}
