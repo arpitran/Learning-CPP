6b9b53ad63d478facd6fe85f80a115833776a4d6
arpitran <35965795+arpitran@users.noreply.github.com>
2019-07-24 03:32:42 -0400

    Create logicalOperators.cpp

---
+++ Operators/logicalOperators.cpp
@@ -0,0 +1,16 @@
+// && AND || OR
+
+#include <cstdio>
+using namespace std;
+
+int main(){
+  bool a = true;
+  bool b = false;
+
+  if(!(a||b)){
+    puts("true");
+  }else{
+    puts("false");
+  }
+  return  0;
+}
