35982c1f1c6e70a4c6525870ec4638ebae099335
arpitran <35965795+arpitran@users.noreply.github.com>
2019-07-24 21:26:41 -0400

    Update sizeof.cpp

--- Operators/sizeof.cpp
+++ Operators/sizeof.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 int main(){
-  int x = 5;
-  size_t y = sizeof x;
-  printf("size of x is %zd\n",y );
+  struct X{
+    int a;
+    int b;
+    char c;
+    char d;
+    long long int e;
+    long long int f;
+  };
+
+  printf("size of X is %zd\n",sizeof (X) );
 
   return 0;
 
