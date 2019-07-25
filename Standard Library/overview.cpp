a8ca6f8396c83f7934a607f1d8b2cfa0ba5e969b
arpitran <35965795+arpitran@users.noreply.github.com>
2019-07-25 19:39:03 -0400

    Create overview.cpp

---
+++ Standard Library/overview.cpp
@@ -0,0 +1,24 @@
+// C-strings
+/*Date and time
+Math, Localization, Process control
+
+Headers
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cerrno>
+
+using namespace std;
+
+std::Puts("Hello, World!")
+
+Well documented
+
+Standard functions
+Some have different versions
+
+Standard Library is very large
+Many good references to cover all Library
+
+*/
