763ad9198c72968d8e5288d3d1c92484038d17e6
arpitran <35965795+arpitran@users.noreply.github.com>
2019-07-26 02:48:46 -0400

    Create queues-string-to-uppercase.cpp

---
+++ Nail Your C++ Interview/queues-string-to-uppercase.cpp
@@ -0,0 +1,26 @@
+/*
+Challenge Question
+Convert a string to uppercase, one character at a time, in order.
+
+*/
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+  string toConvert = "this is the string to convert";
+  queue <char> converted;
+
+  for (int i=0;i<toConvert.length();i++)
+  {
+    converted.push(toupper(toConvert[i]));
+  }
+  for (int i=0;i<toConvert.length();i++)
+  {
+    cout<<converted.front();
+    converted.pop();
+  }
+  return 0;
+}
