e893ce248a16d411ea27da7775927a23e75337ef
arpitran <35965795+arpitran@users.noreply.github.com>
2019-07-25 22:20:12 -0400

    Update file-management.cpp

--- Standard Library/file-management.cpp
+++ Standard Library/file-management.cpp
@@ -3,8 +3,17 @@ using namespace std;
 
 int main(){
   static const char * fn1 = "file1";
+  static const char * fn2 = "file2";
+
+
   FILE * fh = fopen(fn1,"w");
   fclose(fh);
+  puts("Closed file fn1\n");
+
+  rename(fn1,fn2); // rename file
+  puts("renamed file fn1 to fn2\n");
+  remove(fn2);  // remove file
+  puts("removed file fn2\n");
   puts("Done.");
   return 0;
 }
